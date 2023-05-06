745e9b89c1977b4454f7d46536a1259a3b93ac8e
technosirius <tnda108@gmail.com>
2023-05-06 22:42:47 +0300

    fix project reorganized

--- src/App.js
+++ src/App.js
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-import { Configuration, OpenAIApi } from 'openai';
-
-import FormSection from './openai/FormSection';
-import AnswerSection from './openai/AnswerSection';
-import { useState } from 'react';
-
-const App = () => {
-
-  const key = "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"
-
-  const configuration = new Configuration({
-    apiKey: key
-  })
-
-  const openai = new OpenAIApi(configuration)
-
-  const [storedValues, setStoredValues] = useState([]);
-
-  const generateResponse = async (newQuestion, setNewQuestion) => {
-        let options = {
-            model: 'text-davinci-003',
-            temperature: 0,
-            max_tokens: 100,
-            top_p: 1,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-            stop: ['/'],
-        };
-
-        let completeOptions = {
-            ...options,
-            prompt: newQuestion,
-        };
-
-        const response = await openai.createCompletion(completeOptions);
-
-        // console.log(response.data.choices[0].text);
-        if (response.data.choices) {
-            setStoredValues([
-                {
-                    question: newQuestion,
-                    answer: response.data.choices[0].text,
-                },
-                ...storedValues,
-            ]);
-            setNewQuestion('');
-        }
-  };
-
-
-    return (
-        <div>
-            <div className="header-section">
-                <h1>ChatGPT CLONE 🤖</h1>
-                <p>
-                    I am an automated question and answer system, designed to assist you
-                    in finding relevant information. You are welcome to ask me any queries
-                    you may have, and I will do my utmost to offer you a reliable
-                    response. Kindly keep in mind that I am a machine and operate solely
-                    based on programmed algorithms.
-                </p>
-            </div>
-
-            <FormSection generateResponse={generateResponse} />
-            <AnswerSection storedValues={storedValues} />
-        </div>
-    );
-};
-=======
 import { React, Suspense, StrictMode, lazy } from 'react';
 
 // Route librairies import
@@ -106,6 +36,5 @@ function App() {
     </StrictMode>
   );
 }
->>>>>>> e888908428a4e089a1ea93a98453ccef3aa9ac5e
 
 export default App;
