6a22d765eb9ce1486d518138c79568819659fafb
EdwardStevano <edouardostevano@gmail.com>
2023-05-06 23:15:38 -0700

    Begin mainPage integration

---
+++ src/pages/mainPage/components/mainpage_navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function mainpage_navbar() {
+  return (
+    <div className='mainpage_Navbar'>
+        Navbar
+    </div>
+  )
+}
+
+export default mainpage_navbar
--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
+// Pages import
+import Mainpage_navbar from './components/mainpage_navbar'
+
+// Asset import
+// import 
+
 function mainPage() {
   return (
-    <div>mainPage</div>
+    <div className='mainPage_container'>
+      
+      <Parallax pages={5}>
+        <Mainpage_navbar/>
+
+        <ParallaxLayer
+        offset={0}
+        speed={1}
+        >
+          <h1>TEST1</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1}>
+          <h1>TEST2</h1>
+        </ParallaxLayer> 
+      </Parallax>
+
+    </div>
   )
 }
 
